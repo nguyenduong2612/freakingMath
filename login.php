711a2232bf5e47bc7842df821c2f21a3aade8b49
Dương Hải Nguyên <36105378+nguyenduong2612@users.noreply.github.com>
2018-03-27 03:01:51 +0700

    update freaking math v1.4

--- login.php
+++ login.php
@@ -29,12 +29,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
 	<title>Login</title>
 	<meta charset="utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="login.css">
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+	<div class="loading">
+		<span class="intro">FREAKING MATH</span>
+		<span class="credit">Group 22 's project</span>
+	</div>
 	<div class="main">
+		<img src="sub_right_chara4.png" width="260" height="369">
 		<span class="head">FREAKING MATH</span>
 		<div class="login">
 			<form action = "" method = "post">
@@ -42,8 +48,14 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 				<label>Password  :</label><input type = "password" name = "password" class = "box" size="25"><br><br>
 				<input type = "submit" value = " Sign in " class="submit	"><br>
 			</form>
-			<span class="signup">New player? Sign up <a href="register.php">here</a></span>			
+			<span class="signup">New player? Sign up <a href="register.php">here</a></span>		
 		</div>
 	</div>
+
+	<script>
+		$(".loading").delay(2500).fadeOut(1000);
+		$(".login").hide();
+		$(".login").delay(3000).fadeIn(1500);
+	</script>
 </body>
 </html>
--- register.php
+++ register.php
@@ -8,27 +8,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 	$newusername = $_POST['username'];
 	$newpassword = $_POST['password'];
 	$repassword = $_POST['repassword'];
+	$lenuser = strlen($_POST['username']);
+	$lenpass = strlen($_POST['password']);
 
-	if ($newpassword != $repassword) {
+	if (($lenuser == 0) || ($lenpass == 0))
+		echo "<span class='fail'>Please input your username and password</span>";
+	else if ($newpassword != $repassword)
 		echo "<span class='fail'>Retype password error</span>";
-	} else {
+	else {
 		$sql = "INSERT INTO user (username, password, highscore) VALUES ('$newusername', '$newpassword', '0')";
 		$result = mysqli_query($conn ,$sql);
 		if ($result) {
 			header("Location: success.php");
-		} else $conn->error;
+		} else 
+			$conn->error;
 	}
 }
+
 ?>
 
 <html>
 <head>
 	<title>Sign Up</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="register.css">
 	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
+	<img src="sub_right_chara5.png" width="260" height="369">
 	<div class="main">
 		<span class="fail"><?php echo $conn->error; ?></span>
 		<span class="head">Sign up</span>
@@ -41,5 +49,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 			</form>		
 		</div>
 	</div>
+
 </body>
 </html>
